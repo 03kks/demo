32696b78873863baaf0ca788090a8d81159bf26e
krishna kumar singh <03kksingh@gmail.com>
2022-04-22 15:41:30 +0530

    initial commit

---
+++ src/kks.java
@@ -0,0 +1,19 @@
+public class kks {
+    public static void main(String[] args) {
+        int marks = 900;
+        System.out.println("The marks are: " + marks);
+        marks = 1000;
+        System.out.println("The marks are: " + marks);
+        String name="pk";
+        System.out.println("student name is: " +name);
+
+        int a=400,b=500;
+
+        System.out.println(a+b);
+
+        double d=500.66552365265;
+        System.out.println(a+b+d);
+
+    }
+
+}
---
+++ src/kks1.java
@@ -0,0 +1,10 @@
+
+public class kks1 {
+    public static void main(String[] args) {
+        int i = 2,j=100;
+        while(i<=20)
+        { System.out.println(i+","+j); i+=2; }
+
+
+    }
+}
